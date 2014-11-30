bc85a057dbf4facbb3dd24183fb845bbfe95c4c2
Philipp Tauber <pptauber@gmail.com>
2014-11-30 16:56:27 +0100

    added files created by m.kern

---
+++ js/impfstellen.js
@@ -0,0 +1,49 @@
+ $(init)
+
+function init() {
+
+    //get data
+    var options = { url: "http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:IMPFSTELLEOGD&srsName=EPSG:4326&outputFormat=json" };
+    $.ajax(options).done(onReceiveComplete);
+}
+
+ function onReceiveComplete(data) {
+     console.log(data);
+
+     $.each(data.features, function(i, value) {
+         var newItem = $("<li>"+ value.properties.BEZEICHNUNG +"</li>");
+         $("#impfstellen").prepend(newItem);
+
+         //google map
+         /*var myLatlng = new google.maps.LatLng(48.209272,16.37280); //vienna
+         var mapOptions = {
+             zoom: 4,
+             center: myLatlng
+         };
+         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+         var myLatlng = new google.maps.LatLng(value.geometry.coordinates[0],value.geometry.coordinates[1]);
+         var marker = new google.maps.Marker({
+             position: myLatlng,
+             map: map,
+             title: value.properties.BEZEICHNUNG
+         });*/
+     });
+ }
+
+ function initialize() {
+     var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+     var mapOptions = {
+         zoom: 4,
+         center: myLatlng
+     }
+     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+     var marker = new google.maps.Marker({
+         position: myLatlng,
+         map: map,
+         title: 'Hello World!'
+     });
+ }
+
+ google.maps.event.addDomListener(window, 'load', initialize);
