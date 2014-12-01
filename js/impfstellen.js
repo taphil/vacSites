6b04d35cb9d586319bb4c2a5a64d20539c1e519f
Philipp Tauber <pptauber@gmail.com>
2014-12-01 20:15:07 +0100

    fixed wrong google map handling

--- js/impfstellen.js
+++ js/impfstellen.js
@@ -10,25 +10,29 @@ function init() {
 
  function onReceiveComplete(data) {
      console.log(data);
-
+     var myLatlng = new google.maps.LatLng(48.209272,16.37280); //vienna
+         var mapOptions = {
+             zoom: 4,
+             center: myLatlng
+         };
+	 var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
      $.each(data.features, function(i, value) {
          var newItem = $("<li>"+ value.properties.BEZEICHNUNG + "  - " + value.properties.WEBLINK1 + "</li>");
          $("#impfstellen").prepend(newItem);
 
          //google map
-         var myLatlng = new google.maps.LatLng(48.209272,16.37280); //vienna
-         var mapOptions = {
-             zoom: 4,
-             center: myLatlng
-         };
-         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+         
+         
 
-         var myLatlng = new google.maps.LatLng(value.geometry.coordinates[0],value.geometry.coordinates[1]);
+         var myLatlng = new google.maps.LatLng(value.geometry.coordinates[1],value.geometry.coordinates[0]);
+         console.log(myLatlng);
+         
          var marker = new google.maps.Marker({
              position: myLatlng,
              map: map,
              title: value.properties.BEZEICHNUNG
          });
+         console.log(marker);
      });
  }
 
