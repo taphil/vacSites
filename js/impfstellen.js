50edcf0c3f370e043ffad4c85ee61f899c098549
Philipp Tauber <pptauber@gmail.com>
2014-12-03 12:42:27 +0100

    styling

--- js/impfstellen.js
+++ js/impfstellen.js
@@ -1,12 +1,16 @@
-$(init)
-
+/*call the init-function*/
+$(init);
+var map;
 function init() {
-	console.log("in init function");
-    //get data
+	/*add event listeners*/
+	$('#btnGetPosition').click(getCurrentPosition);
+	
+    /*get data from open data service*/
     var options = { url: "http://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:IMPFSTELLEOGD&srsName=EPSG:4326&outputFormat=json" };
     $.ajax(options).done(onReceiveComplete);
-    initialize();
-    $('#btnGetPosition').click(getCurrentPosition);
+    
+    /*initialize the google map*/
+    initializeMap();
 }
 
  function onReceiveComplete(data) {
@@ -16,7 +20,7 @@ function init() {
              zoom: 4,
              center: myLatlng
          };
-	 var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	 map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
      $.each(data.features, function(i, value) {
          var newItem = $("<li>"+ value.properties.BEZEICHNUNG + "  - " + value.properties.WEBLINK1 + "</li>");
          $("#impfstellen").prepend(newItem);
@@ -33,11 +37,21 @@ function init() {
              map: map,
              title: value.properties.BEZEICHNUNG
          });
+         
+         var infowindow = new google.maps.InfoWindow({
+      		content: "<p>" + value.properties.BEZEICHNUNG + "</p><a href=\"" + value.properties.WEBLINK1 + "\">Weblink</a>"
+  		});
+
+         
+         google.maps.event.addListener(marker, 'click', function() {
+    		infowindow.open(map, marker);
+  		});
+
          console.log(marker);
      });
  }
 
- function initialize() {
+ function initializeMap() {
  	 console.log("initialize map");
      var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
      var mapOptions = {
@@ -78,6 +92,9 @@ function showPositionOnMap(position) {
              zoom: 14,
              center: myLatlng
          };
-	 var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+     console.log(map);
+     map.setOptions(mapOptions);
+	 //var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	 
 }
 //google.maps.event.addDomListener(window, 'load', initialize);
