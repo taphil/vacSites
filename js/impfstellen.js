c7bd20a0165e26158ff19dd78506f446183003bf
matthias <s1310307018@students.fh-hagenberg.at>
2014-12-09 20:25:48 +0100

    impressum added (with generator) first tries with header/footer and less

--- js/impfstellen.js
+++ js/impfstellen.js
@@ -1,6 +1,9 @@
 /*call the init-function*/
 $(init);
+
 var map;
+var vacs;
+
 function init() {
 	/*add event listeners*/
 	$('#btnGetPosition').click(getCurrentPosition);
@@ -14,6 +17,8 @@ function init() {
 }
 
  function onReceiveComplete(data) {
+     vacs = data;
+
      console.log(data);
      var myLatlng = new google.maps.LatLng(48.209272,16.37280); //vienna
          var mapOptions = {
@@ -95,6 +100,34 @@ function showPositionOnMap(position) {
      console.log(map);
      map.setOptions(mapOptions);
 	 //var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	 
+
+    var shortestDistance = 999999;
+    var currentDistance;
+    $.each(vacs.features, function(i, value) {
+        currentDistance = distance(value.geometry.coordinates[1],value.geometry.coordinates[0], position.coords.latitude, position.coords.longitude);
+        if (currentDistance < shortestDistance) {
+            shortestDistance = currentDistance;
+        }
+    });
+
+    alert("shortest distance: " + shortestDistance);
 }
 //google.maps.event.addDomListener(window, 'load', initialize);
+
+function distance(lat1, lon1, lat2, lon2) {
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var radlon1 = Math.PI * lon1/180;
+    var radlon2 = Math.PI * lon2/180;
+
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344;
+    return dist;
+}
