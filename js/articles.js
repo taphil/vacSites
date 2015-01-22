77b081cfa1b3d3cef646bc19e98fd6e6ee782f30
Philipp Tauber <pptauber@gmail.com>
2015-01-22 14:54:38 +0100

    expanded expander

--- js/articles.js
+++ js/articles.js
@@ -3,15 +3,20 @@ $(initArticles);
 function initArticles() {
 	$(".action-button").on("click", function(event) {
 		event.preventDefault();
-		alert("here");
-		console.log($(this).closest(".info-article").children(".article-content"));
+		//alert("here");
+		//console.log($(this).closest(".info-article").children(".article-content"));
+		
 		var articleContent = $(this).closest(".info-article").children(".article-content");
-		console.log(articleContent);
-		$(articleContent).removeClass("hidden");
-		$(articleContent).addClass("visible");
+		//console.log(articleContent);
+		/*$(articleContent).removeClass("hidden");
+		$(articleContent).addClass("visible");*/
+		$(articleContent).toggleClass("hidden visible");
+		
 		var infoArticle = $(this).closest(".info-article");
-		$(infoArticle).removeClass("collapsed");
-		$(infoArticle).addClass("expanded");
-		$(this).html("Hide");
+		/*$(infoArticle).removeClass("collapsed");
+		$(infoArticle).addClass("expanded");*/
+		$(infoArticle).toggleClass("collapsed expanded");
+		
+		$(this).html(($(this).html() == "Hide" ? "Show" : "Hide"));
 	});
 }
