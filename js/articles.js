984f3eb8140dfbc1baba850c98cc9b3f70711323
Philipp Tauber <pptauber@gmail.com>
2015-02-10 00:43:41 +0100

    layout work

--- js/articles.js
+++ js/articles.js
@@ -1,22 +1,30 @@
 $(initArticles);
 
 function initArticles() {
-	$(".action-button").on("click", function(event) {
-		event.preventDefault();
-		//alert("here");
-		//console.log($(this).closest(".info-article").children(".article-content"));
-		
-		var articleContent = $(this).closest(".info-article").children(".article-content");
-		//console.log(articleContent);
-		/*$(articleContent).removeClass("hidden");
-		$(articleContent).addClass("visible");*/
-		$(articleContent).toggleClass("hidden visible");
-		
-		var infoArticle = $(this).closest(".info-article");
-		/*$(infoArticle).removeClass("collapsed");
-		$(infoArticle).addClass("expanded");*/
-		$(infoArticle).toggleClass("collapsed expanded");
-		
-		$(this).html(($(this).html() == "Hide" ? "Show" : "Hide"));
-	});
+	$(".action-button").on("click", toggleExpander);
+	$(".control-img").on("click", toggleExpander);
+	$(".article-header").on("click", toggleExpander);
+}
+
+function toggleExpander(event) {
+	event.preventDefault();
+	
+	var articleContent = $(this).closest(".info-article").children(".content-div");
+	$(articleContent).toggleClass("content-hidden content-visible");
+
+	var infoArticle = $(this).closest(".info-article");
+	$(infoArticle).toggleClass("collapsed expanded");
+
+	var controlImg = $(this).closest(".info-article").find(".control-img");
+	$(controlImg).attr('src', ($(controlImg).attr('src') === 'img/tiny-arrow-top-double-512.png' ? 'img/tiny-arrow-bottom-double-512.png' : 'img/tiny-arrow-top-double-512.png'));
+	
+	var expandButton = $(this).closest(".info-article").find(".action-button");
+	$(expandButton).html(($(expandButton).html() === "Hide" ? "Show" : "Hide"));
+	
+	var videoPlayers = $(this).closest(".info-article").find("video");
+	if ($(this).html() === "Hide") {
+		$.each(videoPlayers, function(index, video) {
+			video.pause();
+		});
+	}
 }
